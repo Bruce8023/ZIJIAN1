766730e0287088ccaa228147475277a59cf8ae82
Bruce8023 <76401355+Bruce8023@users.noreply.github.com>
2021-07-10 21:28:11 +0800

    Add files via upload

---
+++ Task/jrttCOOKIE.js
@@ -0,0 +1,24 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let signurlVal= ``
+
+let signkeyVal= ``
+
+let farmurlVal= ``
+
+let farmkeyVal= ``
+
+let readurlVal= ``
+
+let readkeyVal= ``
+
+let jrttcookie = {
+  signurlVal: signurlVal,
+  signkeyVal: signkeyVal,
+  farmurlVal: farmurlVal,
+  farmkeyVal: farmkeyVal,
+  readurlVal: readurlVal,
+  readkeyVal: readkeyVal,  
+}
+
+module.exports =  jrttcookie
+  
